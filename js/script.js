3c6bdd801e98d512ed2b2fdec0e31590ebc84045
Osvaldo Andrade <98958056+AndradeOsvaldo@users.noreply.github.com>
2022-03-22 12:22:54 +0000

    Textos

--- js/script.js
+++ js/script.js
@@ -72,6 +72,7 @@ var runWinSequence = function() {
       if(player === 1 || player === 2 && aiMode === false) {
         $('.heap-two').append("<h3 class='win-msg'>" + playerName + " Ganhou!!</h3>");
       } else {
+        $('.heap-one').append("<img src='img/ai-brain.png' class='brain responsive-img'>");
         $('.heap-two').append("<h3 class='win-msg'>Não foi dessa vez... O computador ganhou!</h3>");
       }
       $('.heap-one').fadeIn("slow") 
